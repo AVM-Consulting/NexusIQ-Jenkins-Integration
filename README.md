aab0e5f9f86e1e81dc719a619dcba30a973dfb96
Shabbir Bata <sbata@pieriandx.com>
2020-10-23 10:03:07 -0500

    initial commit on working branch

--- README.md
+++ README.md
@@ -1,1 +1,48 @@
 # NexusIQ-Jenkins-Integration
+
+## This script helps update multiple Jenkins jobs at once. It enables NexusIQ plugin post build step in Jenkins job. 
+
+### Use case 1:
+
+> use-case-1.groovy file correspond to the following use-case, where we want the integration to be part of all the jobs (after the build step) in all the folders except Project_3_Job_1
+
+```sh
+Project_1
+	Project_1_Job_1
+	Project_1_Job_2
+Project_2
+	Project_2_Job_1
+	Project_2_Job_2
+Project_3
+	Project_3_Job_1
+	Project_3_Job_2
+``` 
+
+#### Key variables for use-case 1:
+- startingPoints: The path of the Jenkins job until the specific folder level from which the script will iterate and scan for the specified conditions to match.
+- excludedPath: The project(s) we wish to exclude from executing.
+
+************************************************************************
+### Use case 2:
+
+> use-case-2.groovy file correspond to the following use-case, where we want the integration to be part of specific jobs (with prefix _build_) inside specific folder(s) only. 
+
+```sh
+Project_1
+	Project_1_Job_1_Build
+	Project_1_Job_2
+Project_2
+	Project_2_Job_1_Build
+	Project_2_Job_2
+Project_3
+	Project_3_Job_3_Build
+	Project_3_Job_3	
+``` 
+
+#### Key variables for use-case 2:
+- job_suffix: Specifies conditions for the job name. 
+- startingPoints:  The path of the Jenkins job until the specific folder level from which the script will iterate and scan for the specified conditions to match.
+- includedPath: The project(s) we wish to include from executing.
+
+
+Reference - https://help.sonatype.com/integrations/nexus-and-continuous-integration/nexus-platform-plugin-for-jenkins
---
+++ use-case-1.groovy
@@ -0,0 +1,77 @@
+import hudson.model.*
+import jenkins.model.Jenkins
+import java.io.File;
+import groovy.xml.XmlUtil
+import static javax.xml.xpath.XPathConstants.*
+import java.io.InputStream;
+import java.io.FileInputStream
+import javax.xml.transform.stream.StreamSource
+import groovy.util.XmlParser
+
+def startingPoints=["Most_Granular_Level_Of_Jenkins_Folder"]
+def excludedPath = ["Project_3"]
+
+def allProject = []
+def excludedProjects = []
+
+Jenkins.instance.getAllItems(AbstractProject.class).each{item -> 
+  def startingPoint = startingPoints.each{ start -> 
+    if(item.fullName.toLowerCase().startsWith(start.toLowerCase())){
+      allProject.add(item.fullName.toLowerCase())
+    }
+  }
+}
+
+Jenkins.instance.getAllItems(AbstractProject.class).each{item -> 
+  def startingPoint = excludedPath.each{ start -> 
+    if(item.fullName.toLowerCase().contains(start.toLowerCase())){
+      excludedProjects.add(item.fullName.toLowerCase())
+    }
+  }
+}
+
+allProject.intersect(excludedProjects).each{allProject.remove(it);excludedProjects.remove(it)}
+
+def includedProject= allProject+excludedProjects
+
+def nexusIqBuilder = '''
+<org.sonatype.nexus.ci.iq.IqPolicyEvaluatorBuildStep plugin="nexus-jenkins-plugin@3.9.20200722-164144.e3a1be0">
+<com__sonatype__nexus__ci__iq__IqPolicyEvaluator____iqStage>Build</com__sonatype__nexus__ci__iq__IqPolicyEvaluator____iqStage>
+<com__sonatype__nexus__ci__iq__IqPolicyEvaluator____iqApplication class="org.sonatype.nexus.ci.iq.SelectedApplication">
+<applicationId></applicationId>
+</com__sonatype__nexus__ci__iq__IqPolicyEvaluator____iqApplication>
+<com__sonatype__nexus__ci__iq__IqPolicyEvaluator____failBuildOnNetworkError>false</com__sonatype__nexus__ci__iq__IqPolicyEvaluator____failBuildOnNetworkError>
+<com__sonatype__nexus__ci__iq__IqPolicyEvaluator____jobCredentialsId></com__sonatype__nexus__ci__iq__IqPolicyEvaluator____jobCredentialsId>
+<advancedProperties></advancedProperties>
+</org.sonatype.nexus.ci.iq.IqPolicyEvaluatorBuildStep>
+
+'''
+Jenkins.instance.getAllItems(AbstractProject.class).each {item ->
+  includedProject.each{include -> 
+    if(item.fullName.toLowerCase() == include.toLowerCase()){
+      def config = item.getConfigFile()
+      File file = config.getFile()
+      String fileContents = file.getText('UTF-8')
+      xml=new XmlParser().parseText(fileContents)
+      
+      def nodeToModify=xml.builders.findAll{n-> 
+          if(! n."org.sonatype.nexus.ci.iq.IqPolicyEvaluatorBuildStep"){
+              newBuilder = new XmlParser( false, true ).parseText( nexusIqBuilder )
+              xml.find { it.name() == 'builders' }.children().add( 0, newBuilder )
+              file.withWriter { w ->
+                  w.write(XmlUtil.serialize(xml))
+              }
+              // reload jenkins job config file
+              InputStream is = new FileInputStream(file)
+              item.updateByXml(new StreamSource(is))
+              item.save()
+              }     
+      }
+      print("NexusIQ plugin added to "+ item.fullName +" in the build step\n")
+    }
+    else{
+        print("NexusIQ plugin skipped in "+ item.fullName +"\n")
+    }
+  }
+}
+
